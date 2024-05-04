767c969328f41f5f5c42a5d6ac891a37cbde96ba
Le Hong Quan <quanjoker25998@gmail.com>
2024-05-05 01:07:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<div align="center">
+    <a href="https://hongquanle.com"><h1 align="center">hongquanle.com</h1></a>
+
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com/), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+[![Deploy with Vercel](https://vercel.com/button)]()
+
+## Running Locally
+
+```sh-session
+git clone https://quanlehongit@dev.azure.com/quanlehongit/Portfolio/_git/Portfolio
+cd Portfolio
+```
+
+Create a `.env` file similar to [`.env.example`]().
+
+Then install dependencies and run the development server:
+
+```sh-session
+bun install
+bun dev
+```
+
+## Cloning / Forking
+
+Please remove all of my personal information (projects, images, etc.) before deploying your own version of this site.
---
+++ app/layout.tsx
@@ -0,0 +1,72 @@
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import "../global.css";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "hongquanle.com",
+    template: "%s | hongquanle.com",
+  },
+  description: "Fullstack developer",
+  openGraph: {
+    title: "hongquanle.com",
+    description: "Fullstack developer",
+    url: "https://hongquanle.com",
+    siteName: "hongquanle.com",
+    images: [
+      {
+        url: "https://hongquanle.com/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "vi-VN",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
