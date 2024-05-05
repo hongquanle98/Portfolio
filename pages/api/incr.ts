0d6ac505333e379d39e7ad81bf52c0b9b3469e2e
Le Hong Quan <quanjoker25998@gmail.com>
2024-05-05 13:00:03 +0700

    log fix deduplicate

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -23,6 +23,8 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
 		return new NextResponse("Slug not found", { status: 400 });
 	}
 	const ip = req.ip;
+	console.log("req", req);
+	
 	if (ip) {
 		// Hash the IP in order to not store it directly in your db.
 		const buf = await crypto.subtle.digest(
@@ -33,14 +35,17 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
 			.map((b) => b.toString(16).padStart(2, "0"))
 			.join("");
 
+		const isDup = await redis.get(["deduplicate", hash, slug].join(":"));
+		console.log("isDup", isDup);
+		if (isDup) {
+			return new NextResponse("recently viewed", { status: 202 });
+		}
+
 		// deduplicate the ip for each slug
-		const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
+		await redis.set(["deduplicate", hash, slug].join(":"), true, {
 			nx: true,
 			ex: 24 * 60 * 60,
 		});
-		if (!isNew) {
-			new NextResponse(null, { status: 202 });
-		}
 	}
 	await redis.incr(["pageviews", "projects", slug].join(":"));
 	return new NextResponse(null, { status: 202 });
